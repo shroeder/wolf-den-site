1566c3b346f6eac760d13c71350abd3f73b5f4de
shroeder <deagle.shroeder@gmail.com>
2026-04-29 23:38:23 -0500

    Add /discord redirect to Discord invite

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/discord',
+        destination: 'https://discord.gg/Pad8U2KVsD',
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
