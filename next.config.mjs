9d8748222adff15a34158781438cf222f29d9b7f
shroeder <deagle.shroeder@gmail.com>
2026-05-24 18:32:39 -0500

    Add permanent apex-to-www redirect

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,18 @@ const nextConfig = {
     async redirects() {
         return [
             {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'header',
+                        key: 'host',
+                        value: 'wolfdengamingmn.com',
+                    },
+                ],
+                destination: 'https://www.wolfdengamingmn.com/:path*',
+                permanent: true,
+            },
+            {
                 source: '/discord',
                 destination: 'https://discord.gg/Pad8U2KVsD',
                 permanent: false,
